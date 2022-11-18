97ce3ec12cb8b53230adc22fbd3127e505362769
bonnyiype <bonnyiype@gmail.com>
2022-11-18 10:52:23 +0000

    Game Scoreboard

---
+++ index.js
@@ -0,0 +1,53 @@
+
+let team1 = 0;
+let team2 = 0;
+teamOne = document.getElementById("team1");
+teamTwo = document.getElementById("team2");
+
+//Team 1 JS Modify
+
+function team1IncrementByOne() {
+    
+    team1 += 1
+    teamOne.textContent = team1
+    
+}
+
+function team1IncrementByTwo() {
+    
+    team1 += 2
+    teamOne.textContent = team1
+    
+}
+
+
+function team1IncrementByThree() {
+    
+    team1 += 3
+    teamOne.textContent = team1
+    
+}
+
+
+
+//Team 2 JS Modify
+
+function team2IncrementByOne() { 
+     team2 += 1
+    teamTwo.textContent = team2
+    
+}
+    
+
+function team2IncrementByTwo() { 
+     team2 += 2
+    teamTwo.textContent = team2
+    
+}
+
+
+function team2IncrementByThree() { 
+     team2 += 3
+    teamTwo.textContent = team2
+    
+}
